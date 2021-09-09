b4a6c4dfb1c03f3b5b9644af8e067fd74bbb84c3
anapimolodec <aiaru.mukhamedyarova>
2021-09-09 18:50:00 +0900

    loading ..

---
+++ script.js
@@ -0,0 +1,15 @@
+
+
+
+	// const MakeGet = (ip) => {
+	// 	let url = "https://geo.ipify.org/api/v1?apiKey=" + "at_9j6iDh1a2wutDTj21Cq4jZugV7l3G" + "&ipAddress=" + ip;
+	// 	fetch(url)
+	//     .then(response => response.json())
+	//     .then(res => console.log(res))
+	//     .catch(error => {
+	//         // handle the error
+	//         console.log("Error:" + response)
+	//     });
+	// }
+ //    // var ip = "8.8.8.8";
+ //    // var api_key = "at_9j6iDh1a2wutDTj21Cq4jZugV7l3G";
